8f41b13b42c7d07f797ebb8a0bfc9653ea3056dc
StuartLunt <stuart@inkwazi.net>
2018-09-04 22:20:00 +0200

    Added GPIO setup, import calls, MCP setup

--- Prac4.py
+++ Prac4.py
@@ -1,5 +1,23 @@
-#Test to see that everyting works
+import RPi.GPIO as GPIO
+import Adafruit_MCP3008, time, os, sys
 
 #Open SPI bus
 spi=spidev.SpiDev()
 spi.open(0,0)
+
+#Set up GPIO
+GPIO.setmode(GPIO.BCM)
+
+#Pin definitions
+SPICLK = 11
+SPIMISO = 9
+SPIMOSI = 10
+SPICS = 8
+
+#GPIO setup
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
+
+mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
