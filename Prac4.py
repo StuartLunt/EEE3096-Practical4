1f2195d5b9e4aea2848e00231b74d013a57bfc83
StuartLunt <stuart@inkwazi.net>
2018-09-07 08:52:50 +0200

    Added button detection and functions

--- Prac4.py
+++ Prac4.py
@@ -1,9 +1,14 @@
 import RPi.GPIO as GPIO
-import Adafruit_MCP3008, time, os, sys
+import Adafruit_MCP3008, time, os, sys, spidev
 
 #Open SPI bus
 spi=spidev.SpiDev()
 spi.open(0,0)
+
+#initialise variables
+startTime = time.time()
+period = 0.5
+run = True
 
 #Set up GPIO
 GPIO.setmode(GPIO.BCM)
@@ -16,6 +21,7 @@ SPICS = 8
 switch1 = 14
 switch2 = 15
 switch3 = 18
+switch4 = 17
 
 #GPIO setup
 GPIO.setup(SPIMOSI, GPIO.OUT)
@@ -27,17 +33,44 @@ GPIO.setup(switch2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(switch3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(switch4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#Event detection set up
-GPIO.add_event_detect(switch1, GPIO.FALLING, callback=reset)
+
 
 mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
 values=[0]*8
+
+def reset(channel):
+    os.system('clear')
+    #Reset timer
+
+def frequency(channel):
+    if period == 0.5 :
+        period=1
+    elif period == 1:
+        period = 2
+    else:
+        period = 0.5
+
+def stop(channel):
+    run = not run
+
+def display(channel):
+    #display last 5
+    currenttime = time.strftime("%H:%M:%S", time.localtime())
+
+#Event detection set up
+GPIO.add_event_detect(switch1, GPIO.FALLING, callback=reset, bouncetime=200)
+GPIO.add_event_detect(switch2, GPIO.FALLING, callback=frequency, bouncetime=200)
+GPIO.add_event_detect(switch3, GPIO.FALLING, callback=stop, bouncetime=200)
+GPIO.add_event_detect(switch4, GPIO.FALLING, callback=display, bouncetime=200)
+
+
+
 
 
 
 while True:
     for i in range(8):
-    values[i] = mcp.read_adc(i)
+        values[i] = mcp.read_adc(i)
 time.sleep(0.5)
 print(values)
 
