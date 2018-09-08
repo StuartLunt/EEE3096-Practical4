1a55ef33a66d344dd207f650fae4a07bb8918252
StuartLunt <stuart@inkwazi.net>
2018-09-08 21:14:49 +0000

    Added time control and storage arrays

--- Prac4.py
+++ Prac4.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import Adafruit_MCP3008, time, os, sys, spidev
+from datetime import datetime, timedelta
+from threading import Event
 
 #Open SPI bus
 spi=spidev.SpiDev()
@@ -9,7 +11,12 @@ spi.open(0,0)
 startTime = time.time()
 period = 0.5
 run = True
-currenttime = time.strftime("%H:%M:%S", time.localtime())
+pause = Event()
+timeArray=[]
+timerArray=[]
+potArray=[]
+tempArray=[]
+lightArray=[]
 
 #Set up GPIO
 GPIO.setmode(GPIO.BCM)
@@ -41,7 +48,7 @@ values=[0]*8
 
 def reset(channel):
     os.system('clear')
-    #Reset timer
+    startTime = time.time()
 
 def frequency(channel):
     if period == 0.5 :
@@ -55,7 +62,21 @@ def stop(channel):
     run = not run
 
 def display(channel):
+    dummy = 0
     #display last 5
+    
+def currentTime():
+    return time.strftime("%H:%M:%S", time.localtime())
+
+def timer():
+    sec=seconds=time.time()-startTime
+    return sec
+
+def timerString():
+    sec=timedelta(seconds=time.time()-startTime)
+    d=datetime(1,1,1)+sec
+    s=str(d.hour)+":"+str(d.minute)+":"+str(d.second)
+    return s
 
 #Event detection set up
 GPIO.add_event_detect(switch1, GPIO.FALLING, callback=reset, bouncetime=200)
@@ -64,6 +85,6 @@ GPIO.add_event_detect(switch3, GPIO.FALLING, callback=stop, bouncetime=200)
 GPIO.add_event_detect(switch4, GPIO.FALLING, callback=display, bouncetime=200)
 
 
-for i in range(0,6):
-    print(currenttime)
-    time.sleep(period)
+while run:
+    print(currentTime()+"\t"+timerString())
+    pause.wait(period)
